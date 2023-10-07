fc8d7ec9e1e8e27e859be5a141b867c9522c8466
SajeedAninda <sajaninda@gmail.com>
2023-10-07 22:08:20 +0600

    Created OnAuth State

--- src/Components/Authentication/AuthProvider.jsx
+++ src/Components/Authentication/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 export let AuthContext = createContext(null);
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { app } from './firebase.config';
 const auth = getAuth(app);
 import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
@@ -8,20 +8,39 @@ const googleProvider = new GoogleAuthProvider();
 const gitProvider = new GithubAuthProvider();
 
 const AuthProvider = ({ children }) => {
+    let [signedInUser, setSignedInUser] = useState(null);
+    let [loading, setLoading] = useState(true);
+
     let register = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
     let login = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     let googleLogin = () => {
+        setLoading(true);
         return signInWithPopup(auth, googleProvider)
     }
     let githubLogin = () => {
+        setLoading(true);
         return signInWithPopup(auth, gitProvider)
 
     }
+    let logOut=()=>{
+        return signOut(auth)
+    }
+    useEffect(() => {
+        let unSubscribe = onAuthStateChanged(auth, (user) => {
+            setSignedInUser(user);
+            setLoading(false);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
 
 
@@ -32,7 +51,10 @@ const AuthProvider = ({ children }) => {
         register,
         login,
         googleLogin,
-        githubLogin
+        githubLogin,
+        logOut,
+        loading,
+        signedInUser
     }
     return (
         <AuthContext.Provider value={authorization}>
